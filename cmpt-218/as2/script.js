a1fa3bab294c56e7aea6c309bf6350494b65e99c
jgc11 <jgc11@asb9838nu-b04.csil.sfu.ca>
2018-02-21 18:01:54 -0800

    as2 progress

--- cmpt-218/as2/script.js
+++ cmpt-218/as2/script.js
@@ -5,7 +5,8 @@ function addEntry(entryNum) {
 		alert("Cannot add any more entries.");
 	} else {
 		var newEntry = document.createElement('div');
-		newEntry.innerHTML = "<h2>ENTRY " + (entries + 1) + "</h2> <label for=\"fname\">FIRST NAME:</label> <input id = \"fname\" placeholder='JOHN' name='fname' type='text' /> <br />	 <label for=\"lname\">LAST NAME:</label> <input name='lname' placeholder='SMITH' type='text' /> <br /> <label for=\"bday\">BIRTHDAY:</label> <input name='bday' placeholder='JAN 1 2018' type='text'/> <br />"
+		newEntry.setAttribute("id", "entry" + (entries + 1));
+		newEntry.innerHTML = "<h2>ENTRY " + (entries + 1) + "</h2> <label for=\"fname\">FIRST NAME:</label> <input id = \"fname\" placeholder='JOHN' name='fname' type='text' /> <br />	 <label for=\"lname\">LAST NAME:</label> <input name='lname' placeholder='SMITH' type='text' /> <br /> <label for=\"bday\">BIRTHDAY:</label> <input name='bday' type='date'/> <br /> <label for=\"email\">EMAIL(s):</label> <input name='email' type='email'/> <br/>"
 		document.getElementById(entryNum).appendChild(newEntry);
 		entries++;
 	}
--- cmpt-218/as2/server.js
+++ cmpt-218/as2/server.js
@@ -5,6 +5,7 @@ var url = require('url');
 var qs = require('querystring');
 var script = require('./script.js');
 
+
 var server = http.createServer();
 server.on('request', function(req,res){
 	console.log('request: ', req.url);
@@ -28,7 +29,22 @@ server.on('request', function(req,res){
 		req.on('end', function(){
 			var postObj = qs.parse(body);
 			console.log(postObj);
+			var json = JSON.stringify(postObj);
+			console.log(json);
+			fs.writeFile('./data/users.json', json, 'utf8');
 			res.end();
+		});
+	} else if (req.method === 'GET' && req.url === '/users.html'){
+		fs.readFile("./form.html", function (err, contents) {
+			if (err) {
+				res.writeHead(404);
+				res.write('404 Error');
+				res.end();
+			} else {
+				res.writeHead(200, {"Content-Type": "text/html"});
+				res.write(contents);
+				res.end();
+			}
 		});
 	} else if (req.method === 'GET' && req.url.match(/^\/.+\.js$/)){
 		var jspath = path.join(__dirname,req.url);
@@ -50,3 +66,5 @@ server.on('request', function(req,res){
 	}
 });
 server.listen(12834);
+
+console.log("Running on port 12834!");
