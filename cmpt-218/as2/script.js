51e08cd1d99d432abe3a1f535a3fbf3a3e43a02c
jgc11 <jgc11@asb9804u-d02.csil.sfu.ca>
2018-02-23 09:05:24 -0800

    as2 update

--- cmpt-218/as2/script.js
+++ cmpt-218/as2/script.js
@@ -1,13 +1,36 @@
 var entries = 1;
 var max = 5;
+
 function addEntry(entryNum) {
 	if (entries == max) {
 		alert("Cannot add any more entries.");
 	} else {
 		var newEntry = document.createElement('div');
 		newEntry.setAttribute("id", "entry" + (entries + 1));
-		newEntry.innerHTML = "<h2>ENTRY " + (entries + 1) + "</h2> <label for=\"fname\">FIRST NAME:</label> <input id = \"fname\" placeholder='JOHN' name='fname' type='text' /> <br />	 <label for=\"lname\">LAST NAME:</label> <input name='lname' placeholder='SMITH' type='text' /> <br /> <label for=\"bday\">BIRTHDAY:</label> <input name='bday' type='date'/> <br /> <label for=\"email\">EMAIL(s):</label> <input name='email' type='email'/> <br/>"
+		newEntry.innerHTML = "<h2>ENTRY " + (entries + 1) + "</h2> <label for=\"fname\">FIRST NAME:</label> <input placeholder='JOHN' name='fname' type='text' /> <br />	 <label for=\"lname\">LAST NAME:</label> <input name='lname' placeholder='SMITH' type='text' /> <br /> <label for=\"bday\">BIRTHDAY:</label> <input name='bday' type='date'/> <br /> <label for=\"email\">EMAIL(s):</label> <input name='email' type='email'/> <br/>"
 		document.getElementById(entryNum).appendChild(newEntry);
 		entries++;
 	}
+}
+
+function loadJSON(callback) {
+	var xobj = new XMLHttpRequest();
+	xobj.overrideMimeType("application/json");
+	xobj.open('GET', './data/users.json', true);
+	xobj.onreadystatechange = function() {
+		if (xobj.readyState == 4 && xobj.status == "200") {
+			callback(xobj.responseText);
+		}
+	};
+}
+
+function init() {
+	var json;
+	loadJSON(function(response) {
+		json = JSON.parse(response);
+	})
+	json.forEach(function() {
+		var tRow = "<tr><td>" + json.users.fname + "</td><td>" + json.users.lname + "</td><td>" + json.users.bday + "</td><td>" + json.users.email + "</td></tr>"
+		tRow.appendTo(document.getElementById(userTable))
+	});
 }
--- cmpt-218/as2/server.js
+++ cmpt-218/as2/server.js
@@ -5,6 +5,8 @@ var url = require('url');
 var qs = require('querystring');
 var script = require('./script.js');
 
+var json = {};
+fs.writeFile('./data/users.json', json, 'utf8');
 
 var server = http.createServer();
 server.on('request', function(req,res){
@@ -23,19 +25,39 @@ server.on('request', function(req,res){
 		});
 	} else if (req.method === 'POST' && req.url === '/') {
 		var body = '';
+
 		req.on('data', function(data){
 			body += data.toString();
 		});
 		req.on('end', function(){
 			var postObj = qs.parse(body);
 			console.log(postObj);
-			var json = JSON.stringify(postObj);
+			var ppl = {"users":[]};
+			var len = 0;
+			try {
+				postObj.fname.forEach(function() {
+					len++;
+				})
+			}
+			catch(err) {
+				len = 1;
+			}
+			if (len>1) {
+				for (var i=0;i<len;i++) {
+					var person = {"fname": postObj.fname[i], "lname": postObj.lname[i], "bday": postObj.bday[i], "email": postObj.email[i]};
+					ppl.users.push(person);
+				}
+			} else {
+				var person = {"fname": postObj.fname, "lname": postObj.lname, "bday": postObj.bday, "email": postObj.email};
+				ppl.users.push(person);
+			}			
+			json = JSON.stringify(ppl);
 			console.log(json);
 			fs.writeFile('./data/users.json', json, 'utf8');
 			res.end();
 		});
 	} else if (req.method === 'GET' && req.url === '/users.html'){
-		fs.readFile("./form.html", function (err, contents) {
+		fs.readFile("./users.html", function (err, contents) {
 			if (err) {
 				res.writeHead(404);
 				res.write('404 Error');
@@ -49,6 +71,19 @@ server.on('request', function(req,res){
 	} else if (req.method === 'GET' && req.url.match(/^\/.+\.js$/)){
 		var jspath = path.join(__dirname,req.url);
 		fs.readFile(jspath, function(err, contents){
+			if (err) {
+				res.writeHead(404);
+				res.write('404 Error');
+				res.end();
+			} else {
+				res.writeHead(200, {"Content-Type": "text/javascript"});
+				res.write(contents);
+				res.end();
+			}
+		})
+	} else if (req.method === 'GET' && req.url.match(/^\/.+\.json$/)){
+		var jsonpath = path.join(__dirname,req.url);
+		fs.readFile(jsonpath, function(err, contents){
 			if (err) {
 				res.writeHead(404);
 				res.write('404 Error');
