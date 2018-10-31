f1b11a90f74774cd7f9e02f2c2d074c25d3de70e
jgc11 <jackiegenechan@gmail.com>
2018-10-30 19:13:20 -0700

    First commit for prj4

---
+++ cmpt-300/prj4/smartLock.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "smartLock.h"
+                           
+int counter = 0;
+
+void init_lock(SmartLock* lock) {
+    pthread_mutex_init(&(lock->mutex), NULL);
+    lock->lockId = counter;
+    counter++;
+}
+
+int lock(SmartLock* lock) {
+	printf("%lu locking\n", pthread_self());
+    pthread_mutex_lock(&(lock->mutex));
+    return 1;
+}
+
+void unlock(SmartLock* lock) {
+    pthread_mutex_unlock(&(lock->mutex));
+}
+
+/*
+ * Cleanup any dynamic allocated memory for SmartLock to avoid memory leak
+ * You can assume that cleanup will always be the last function call
+ * in main function of the test cases.
+ */
+void cleanup() {
+
+}
---
+++ cmpt-300/prj4/smartLock.h
@@ -0,0 +1,19 @@
+#ifndef SMARTLOCK_H
+#define SMARTLOCK_H
+#include <pthread.h>
+
+typedef struct {
+    pthread_mutex_t mutex;
+    int lockId;
+    /* Add other variables */
+} SmartLock;
+       
+void init_lock(SmartLock* lock);
+
+int lock(SmartLock* lock);
+
+void unlock(SmartLock* lock);
+
+void cleanup();
+
+#endif
