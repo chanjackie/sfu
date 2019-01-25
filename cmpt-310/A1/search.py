0ff89cced6ae715df03f5aaec883aad99b6713ed
chanjackie <jackiegenechan@gmail.com>
2019-01-24 20:36:24 -0800

    Finished BFS, started A*

--- cmpt-310/A1/search.py
+++ cmpt-310/A1/search.py
@@ -137,11 +137,41 @@ def depthFirstSearch(problem, visited = [], initialState = None, path = []):
     return path
 
 
-def breadthFirstSearch(problem, visited = [], initialState = None, path = []):
+def breadthFirstSearch(problem, visited = [], initialState = None, path = [], candidates = None):
     """Questoin 1.2
      Search the shallowest nodes in the search tree first.
      """
     "*** YOUR CODE HERE ***"
+    # from util import Queue
+    # if (problem.isGoalState(problem.getStartState()) == True):
+    #     return []
+    # if (initialState == None):
+    #     startState = problem.getStartState()
+    # else:
+    #     startState = initialState
+    # visited.append(startState)
+    # successors = problem.getSuccessors(startState)
+    # candidates = Queue()
+    # for i in successors:
+    #     candidates.push(i)
+    # while (not candidates.isEmpty()):
+    #     nextState = candidates.pop()
+    #     if (problem.isGoalState(nextState[0])):
+    #         print("FOUND GOAL")
+    #         path.append(nextState[1])
+    #         return path
+    #     if (not (nextState[0] in visited)):
+    #         result = breadthFirstSearch(problem, visited, nextState[0], path, candidates)
+    #         if (result != []):
+    #             if (result[0] in problem.getSuccessors(nextState[0])):
+    #                 path.insert(0, nextState)
+    #                 return path
+    # if (startState == problem.getStartState()):
+    #     solution = []
+    #     for i in range(len(path)):
+    #         solution.insert(0, path[i][1])
+    #     return solution
+    # return path
     from util import Queue
     if (problem.isGoalState(problem.getStartState()) == True):
         return []
@@ -160,12 +190,26 @@ def breadthFirstSearch(problem, visited = [], initialState = None, path = []):
         if (problem.isGoalState(nextState[0])):
             print("FOUND GOAL")
             path.append(nextState[1])
-            return path
+            return nextState
         if (not (nextState[0] in visited)):
-            if (breadthFirstSearch(problem, visited, nextState[0], path, candidates) != []):
-                path.insert(0, nextState[1])
-                return path
-    return path
+            result = breadthFirstSearch(problem, visited, nextState[0], path, candidates)
+            if (result != []):
+                print(result)
+                print(calcCoord(getOppositeDirection(result[1]), result[0]))
+                print(nextState[0])
+                if (calcCoord(getOppositeDirection(result[1]), result[0]) == nextState[0]):
+                    path.insert(0, nextState[1])    
+                    if (startState == problem.getStartState()):
+                        print(path)
+                        return path
+                    return nextState
+                if (startState == problem.getStartState()):
+                    return path
+                return result
+    if (startState == problem.getStartState()):
+        print(path)
+        return path
+    return result
 
 
 def nullHeuristic(state, problem=None):
@@ -175,11 +219,70 @@ def nullHeuristic(state, problem=None):
     """
     return 0
 
-def aStarSearch(problem, heuristic=nullHeuristic):
+def aStarSearch(problem, heuristic=nullHeuristic, initialState = None,
+                path = [], candidates = None, cost = 0):
     """Question 1.3
     Search the node that has the lowest combined cost and heuristic first."""
     "*** YOUR CODE HERE ***"
+    from util import PriorityQueue
+    if (problem.isGoalState(problem.getStartState()) == True):
+        return []
+    if (initialState == None):
+        startState = problem.getStartState()
+    else:
+        startState = initialState
+    print("fscore: " + str(cost +(heuristic(startState, problem))) + " at " + str(startState))
+    successors = problem.getSuccessors(startState)
+    if (candidates == None):
+        candidates = PriorityQueue()
+    for i in successors:
+        fscore = cost + heuristic(i[0], problem)
+        candidates.update(i, fscore)
+    while (not candidates.isEmpty()):
+        nextState = candidates.pop()
+        if (problem.isGoalState(nextState[0])):
+            print("FOUND GOAL")
+            path.append(nextState[1])
+            return nextState
+        cost += 1
+        result = aStarSearch(problem, heuristic, nextState[0], path, candidates, cost)
+        if (result != []):
+            if (calcCoord(getOppositeDirection(result[1]), result[0]) == nextState[0]):
+                path.insert(0, nextState[1])    
+                if (startState == problem.getStartState()):
+                    return path
+                return nextState
+            if (startState == problem.getStartState()):
+                return path
+            return result
+    if (startState == problem.getStartState()):
+        return path
+    return result
+"""
+def isOppositeDirection(dir1, dir2):
+    if (dir1 == 'West' and dir2 == 'East'): return True
+    if (dir1 == 'East' and dir2 == 'West'): return True
+    if (dir1 == 'South' and dir2 == 'North'): return True
+    if (dir1 == 'North' and dir2 == 'South'): return True
+    return False
+"""
+def getOppositeDirection(direction):
+    if (direction == 'West'): return 'East'
+    if (direction == 'East'): return 'West'
+    if (direction == 'South'): return 'North'
+    if (direction == 'North'): return 'South'
 
+def calcCoord(direction, coord):
+    newCoord = None
+    if (direction == 'East'):
+        newCoord = (int(coord[0])+1, coord[1])
+    elif (direction == 'West'):
+        newCoord = (int(coord[0])-1, coord[1])
+    elif (direction == 'North'):
+        newCoord = (int(coord[0]), coord[1]+1)
+    elif (direction == 'South'):
+        newCoord = (int(coord[0]), coord[1]-1)
+    return newCoord
 
 
 # Abbreviations
