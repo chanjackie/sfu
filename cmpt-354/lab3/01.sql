da859cc02bad4996cb6fb10c0a1188d144c24c8a
jgc11 <jackiegenechan@gmail.com>
2019-04-03 22:30:04 -0700

    354 Lab3 q1

--- cmpt-354/lab3/01.sql
+++ cmpt-354/lab3/01.sql
@@ -1,11 +1,18 @@
 /*For each product type, calculate the number of orders consisting only of products of that type. Return
 the product type and the number of orders.*/
 
-SELECT P.ptype, COUNT(O.ordid)
-FROM Products P, Orders O, Details D
-WHERE NOT EXISTS (
+SELECT P1.ptype, COUNT(O1.ordid)
+FROM Products P1, Orders O1, Details D1
+WHERE D1.ordid = O1.ordid AND D1.pcode = P1.pcode 
+AND NOT EXISTS (
+	SELECT D2.ordid
+	FROM Details D2
+	WHERE D2.ordid = O1.ordid AND D2.pcode != P1.pcode
+)
+GROUP BY P1.ptype;
+/*
+NOT EXISTS (
 	SELECT P1.ptype, O.ordid
 	FROM Products P1, Products P2, Orders O, Details D
 	WHERE O.ordid = D.ordid AND D.pcode = P2.pcode AND P1.ptype != P2.ptype
-)
-GROUP BY P.ptype;
+)*/
