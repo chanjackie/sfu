7d360e7ec84c0634a463c42aa4686d2988b536ad
chanjackie <jackiegenechan@gmail.com>
2020-01-27 20:37:49 -0800

    Started q3 for A1 CMPT 414

--- cmpt-414/a1/a1_3.m
+++ cmpt-414/a1/a1_3.m
@@ -1,1 +1,13 @@
 % 3. Sharpening
+
+peppers = imread('peppers.png');
+flowers = imread('flower-glass.tif');
+
+p2 = rgb2ycbcr(peppers);
+
+blur1 = [0 1 0;1 -4 1;0 1 0];
+blurPeppers = uint8(convn(p2, blur1, 'same'));
+sharpPeppers = 5*p2 - blurPeppers;
+
+subplot(1,2,1), imshow(peppers);
+subplot(1,2,2), imshow(sharpPeppers);
